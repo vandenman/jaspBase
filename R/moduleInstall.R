4197f094e14476aa1eb48bc0ec67c03a16f09d88
Don van den Bergh <donvdbergh@hotmail.com>
2022-06-23 11:44:54 +0200

    set libPaths inside jaspBase

--- R/moduleInstall.R
+++ R/moduleInstall.R
@@ -342,14 +342,17 @@ setupRenv <- function(moduleLibrary) {
 }
 
 #' @export
-installJaspModuleNew <- function(modulePkg, jaspRoot, moduleLibrary, libPathsToUse, repos, updatePackages = Sys.getenv("JASP_UPDATE_PKGS", unset = "false"),
+installJaspModuleNew <- function(modulePkg, jaspRoot, moduleLibrary, libPathsToUse = .libPaths(), repos = getOption("repos"), updatePackages = Sys.getenv("JASP_UPDATE_PKGS", unset = "false"),
                                  recordPackages = "localJasp", respectModuleLockfile = TRUE) {
   assertValidJASPmodule(modulePkg)
 
   r <- getOption("repos")
   r["CRAN"] <- repos
   options(repos = r)
-  # .libPaths(libPathsToUse)
+
+  oldLibPaths <- .libPaths()
+  .libPaths(c(libPathsToUse, .libPaths()))
+  on.exit(.libPaths(oldLibPaths))
 
   setupRenv(moduleLibrary)
 
